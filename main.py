83cc1fdcf507cf974fd3db3a0e03ea8d17eb410e
RonB8 <ronbuaron8@gmail.com>
2024-10-01 18:26:54 +0300

    .

--- main.py
+++ main.py
@@ -2,42 +2,47 @@ import socket
 from Crypto.PublicKey import RSA
 import threading
 import data
+import warnings
+import threading
 
-
-
-
-# def func1(n=0):
-#     print(22)
-
-# t1 = threading.Thread(target=func1, args=(4,))
-# t1.start()
-# t1.join()
-r1 = data.Request(1,1,1,1,1)
-print(r1.REGISTRY)
-
-print("Stam Bdika")
-exit(0)
-
-
-HOST = ''
-PORT = 1234
-
-
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
+def handle_client(conn, addr):
     with conn:
         print('Connected by', addr)
+    
+        data = conn.recv(1024)
+        # text = data.decode("utf-8")
+        text = data
+        print("Received message: " + text)
+        print("Enter message ")
+        reply = input()
+        replydata = bytearray(reply, "utf-8")
+        newdata = bytearray(1024)
+        for i in range(min(len(replydata), len(newdata))):
+            newdata[i] = replydata[i]
+        conn.sendall(newdata)
+
+def start_server():
+    HOST = ''
+    PORT = 1256 #defulte port
+
+    file_name = "port.info"
+    file_name = "ss.txt"
+    try:
+        f = open(file_name, 'r')
+        PORT = int(f.read())
+        f.close()
+    except:
+        warnings.warn(f'The \'{file_name}\' file does not exist')
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+
         while True:
-            data = conn.recv(1024)
-            text = data.decode("utf-8")
-            print("Received message: " + text)
-            print("Enter message ")
-            reply = input()
-            replydata = bytearray(reply, "utf-8")
-            newdata = bytearray(1024)
-            for i in range(min(len(replydata), len(newdata))):
-                newdata[i] = replydata[i]
-            conn.sendall(newdata)
+            conn, addr = s.accept()
+            client_thread = threading.Thread(target=handle_client, args=(conn, addr,))
+            client_thread.start()
+
+
+if __name__ == "__main__":
+    start_server()
